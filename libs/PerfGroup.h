fd8b8e599c5d5fa451693399995c0abea69fba73
Henrique Bucher <hbucher@gmail.com>
2024-03-29 21:41:42 -0500

    Added some performance labels

--- libs/PerfGroup.h
+++ libs/PerfGroup.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <cstdint>
 #include <unordered_map>
 #include "PerfUtils.h"
 
--- libs/PerfUtils.cpp
+++ libs/PerfUtils.cpp
@@ -50,7 +50,17 @@ bool translate(const char *events[], perf_event_attr *evds, size_t size) {
         {"cycles", {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES}},
         {"instructions", {PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS}},
         {"cache-misses", {PERF_TYPE_HW_CACHE, PERF_COUNT_HW_CACHE_MISSES}},
-        {"branch-misses", {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES}}};
+        {"branch-misses", {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES}},
+        {"backend-stall-cycles",
+         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_BACKEND}},
+        {"frontend-stall-cycles",
+         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_STALLED_CYCLES_FRONTEND}},
+        {"pagefaults", {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_PAGE_FAULTS}},
+        {"context-switches", {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_CONTEXT_SWITCHES}},
+        {"cpu-migrations", {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_CPU_MIGRATIONS}},
+        {"pagefaults-minor", {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_PAGE_FAULTS_MIN}},
+        {"pagefaults-major", {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_PAGE_FAULTS_MAJ}},
+        {"alignment-faults", {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_ALIGNMENT_FAULTS}}};
     for (size_t j = 0; j < size; ++j) {
         perf_event_attr &pe(evds[j]);
         memset(&pe, 0, sizeof(pe));
